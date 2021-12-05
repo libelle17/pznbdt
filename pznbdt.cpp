80a3e03a7aff90769ea3b6c99bcae88988835263
Gerald Schade <gerald.schade@gmx.de>
2021-12-05 14:22:42 +0100

    1.Commit

---
+++ pznbdt.cpp
@@ -0,0 +1,221 @@
+// '//α' oder '//ω' als Kommentar sind nur fuer die Verwendung dieses Programms als Programmvorlage wichtig
+const double& versnr= //α
+#include "versdt"
+;
+#include "kons.h"
+#include "DB.h" 
+#include <tiffio.h>
+#define VOMHAUPTCODE // um Funktionsdefinition manchmal mit "__attribute__((weak)) " versehen zu können //ω
+#include "pznbdt.h"
+// fuer verschiedene Sprachen //α
+char const *DPROG_T[T_MAX+1][SprachZahl]={
+	// T_virtVorgbAllg
+	{"virtVorgbAllg()","virtgeneralprefs()"},
+	// T_pvirtVorgbSpeziell
+	{"pvirtVorgbSpeziell()","pvirtspecialprefs()"},
+	// T_virtMusterVorgb
+	{"virtMusterVorgb()","virtsampleprefs"},
+	// T_pvirtvorrueckfragen
+	{"pvirtvorrueckfragen()","pvirtbeforecallbacks()"},
+	// T_virtrueckfragen
+	{"virtrueckfragen()","virtcallbacks()"},
+	// T_virtpruefweiteres
+	{"virtpruefweiteres()","virtcheckmore()"},
+	// T_virtmacherkl_Tx_lgn
+	{"pvirtmacherkl, Tx.lgn: ","pvirtmakeexpl, Tx.lgn: "},
+	//	T_Fehler_beim_Pruefen_von
+	{"Fehler beim Pruefen von: ","Error while examining: "},
+	// T_st_k
+	{"st","st"},
+	// T_stop_l
+	{"stop","stop"},
+	// T_DPROG_anhalten
+	{DPROG " anhalten","stop " DPROG},
+	// T_anhalten
+	{"anhalten()","stop()"},
+	// T_Cron_Aufruf_von
+	{"Cron-Aufruf von '","cron call of '"},
+	// T_gestoppt
+	{"' gestoppt.","' stopped."},
+	// T_n_k
+	{"n","n"},
+	// T_dszahl_l
+	{"dszahl","reccount"},
+	// T_Zahl_der_aufzulistenden_Datensaetze_ist_zahl_statt
+	{"Zahl der aufzulistenden Datensaetze = <zahl> statt","No. of listed entries = <no> instead of"},
+	// T_Datenbank_nicht_initialisierbar_breche_ab
+	{"Datenbank nicht initialisierbar, breche ab","database init failed, stopping"},
+	// T_Fuege_ein
+	{"Fuege ein: ","Inserting: "}, //ω
+	{"",""} //α
+}; // char const *DPROG_T[T_MAX+1][SprachZahl]=
+
+class TxB Tx((const char* const* const* const*)DPROG_T);
+const char sep = 9; // geht auch: "[[:blank:]]"
+const char *logdt="/var/log/" DPROG "vorgabe.log";//darauf wird in kons.h verwiesen;
+pidvec pidw;
+const unsigned ktage=1; // kurzes Intervall fuer Faxtabellenkorrektur, 1 Tag
+const unsigned mtage=30; // mittleres Intervall fuer Faxtabellenkorrektur, 1 Monat
+const unsigned ltage=73000; // langes Intervall fuer Faxtabellenkorrektur, 200 Jahre
+
+using namespace std; //ω
+hhcl::hhcl(const int argc, const char *const *const argv):dhcl(argc,argv,DPROG,/*mitcron*/1) //α
+{
+ // mitcron=0; //ω
+} // hhcl::hhcl //α
+// Hier neue Funktionen speichern: //ω
+//α
+// wird aufgerufen in lauf
+void hhcl::virtVorgbAllg()
+{
+	hLog(violetts+Tx[T_virtVorgbAllg]+schwarz); //ω
+	dhcl::virtVorgbAllg(); //α
+} // void hhcl::virtVorgbAllg
+
+// wird aufgerufen in lauf
+void hhcl::pvirtVorgbSpeziell()
+{
+	hLog(violetts+Tx[T_pvirtVorgbSpeziell]+schwarz);
+	virtMusterVorgb(); //ω
+	dhcl::pvirtVorgbSpeziell(); //α
+} // void hhcl::pvirtVorgbSpeziell
+
+// wird aufgerufen in lauf
+void hhcl::virtinitopt()
+{ //ω
+	opn<<new optcl(/*pptr*/&anhl,/*art*/puchar,T_st_k,T_stop_l,/*TxBp*/&Tx,/*Txi*/T_DPROG_anhalten,/*wi*/1,/*Txi2*/-1,/*rottxt*/nix,/*wert*/1,/*woher*/1); //α //ω
+	opn<<new optcl(/*pptr*/&dszahl,/*art*/pdez,T_n_k,T_dszahl_l,/*TxBp*/&Tx,/*Txi*/T_Zahl_der_aufzulistenden_Datensaetze_ist_zahl_statt,/*wi*/1,/*Txi2*/-1,/*rottxt*/nix,/*wert*/-1,/*woher*/1); //α //ω
+	dhcl::virtinitopt(); //α
+} // void hhcl::virtinitopt
+
+// wird aufgerufen in lauf
+void hhcl::pvirtmacherkl()
+{
+	hLog(violetts+Tx[T_virtmacherkl_Tx_lgn]+schwarz+ltoan(Tx.lgn));
+//	erkl<<violett<<DPROG<<blau<<Txk[T_tut_dieses_und_jenes]<<schwarz; //ω 
+} // void hhcl::pvirtmacherkl //α
+//ω
+//α
+// wird aufgerufen in lauf
+void hhcl::virtMusterVorgb()
+{
+	hLog(violetts+Tx[T_virtMusterVorgb]+schwarz); //ω
+	dhcl::virtMusterVorgb(); //α
+} // void hhcl::MusterVorgb
+
+// wird aufgerufen in lauf
+void hhcl::pvirtvorzaehler()
+{ //ω
+} // void hhcl::virtvorzaehler() //α
+//ω
+// wird aufgerufen in lauf //α
+void hhcl::virtzeigversion(const string& ltiffv/*=nix*/)
+{
+	dhcl::virtzeigversion(ltiffv);  //ω
+} // void hhcl::virtzeigversion //α
+//ω
+//α
+// wird aufgerufen in lauf
+void hhcl::pvirtvorrueckfragen()
+{
+	hLog(violetts+Tx[T_pvirtvorrueckfragen]+schwarz); //ω
+} // void hhcl::pvirtvorrueckfragen //α
+
+//α
+// wird aufgerufen in lauf
+void hhcl::virtpruefweiteres()
+{
+	fLog(violetts+Tx[T_virtpruefweiteres]+schwarz,obverb,oblog); //ω
+	// if (initDB()) exit(schluss(10,Tx[T_Datenbank_nicht_initialisierbar_breche_ab])); //α //ω
+	hcl::virtpruefweiteres(); // z.Zt. leer //α
+} // void hhcl::virtpruefweiteres
+
+// wird aufgerufen in lauf
+void hhcl::virtzeigueberschrift()
+{ //ω
+	// hier ggf. noch etwas an 'uebers' anhaengen //α
+	hcl::virtzeigueberschrift();
+} // void hhcl::virtzeigueberschrift
+//ω
+//α
+// Parameter -st / --stop
+// wird aufgerufen in: main
+void hhcl::anhalten()
+{
+	hLog(violetts+Tx[T_anhalten]+schwarz);
+	// crontab
+	/*
+	setztmpcron();
+	for(int iru=0;iru<1;iru++) {
+		const string befehl=
+			"bash -c 'grep \""+saufr[iru]+"\" -q <(crontab -l)&&{ crontab -l|sed \"/"+zsaufr[iru]+"/d\">"+tmpcron+";crontab "+tmpcron+";};:'";
+		systemrueck(befehl,obverb,oblog,*//*rueck=*//*0,*//*obsudc=*//*1);
+	} // 	for(int iru=0;iru<2;iru++)
+  */
+	pruefcron("0"); // soll vor Log(Tx[T_Verwende ... stehen
+	fLog(blaus+Tx[T_Cron_Aufruf_von]+schwarz+mpfad+blau+Tx[T_gestoppt]+schwarz,1,oblog); //ω
+} // void hhcl::anhalten() //α
+//ω
+//α
+void hhcl::pvirtnachrueckfragen()
+{
+	// if (initDB()) exit(schluss(10,Tx[T_Datenbank_nicht_initialisierbar_breche_ab]));  //ω
+} // void hhcl::pvirtnachrueckfragen //α
+//ω
+void hhcl::pvirtfuehraus() //α
+{ //ω
+} // void hhcl::pvirtfuehraus  //α
+
+
+// wird aufgerufen in lauf
+void hhcl::virtschlussanzeige()
+{   //ω
+	dhcl::virtschlussanzeige(); //α
+} // void hhcl::virtschlussanzeige
+ 
+// wird aufgerufen in: main
+void hhcl::virtautokonfschreib()
+{
+// const int altobverb=obverb;
+// obverb=1;
+	hLog(violetts+Txk[T_autokonfschreib]+schwarz+", "+Txk[T_zu_schreiben]+((rzf||hccd.obzuschreib)?Txk[T_ja]:Txk[T_nein])); //ω
+	struct stat kstat{0}; //α
+	if (lstat(akonfdt.c_str(),&kstat))
+		hccd.obzuschreib=1;
+	if (rzf||hccd.obzuschreib) {
+		hLog(gruens+Txk[T_schreibe_Konfiguration]+schwarz);
+		// restliche Erklaerungen festlegen
+		////    agcnfA.setzbem("language",sprachstr);
+		hcl::virtautokonfschreib(); //ω
+	} // if (rzf||hccd.obzuschreib) //α
+// obverb=altobverb;
+} // void hhcl::virtautokonfschreib
+
+hhcl::~hhcl() 
+{ //ω
+} // hhcl::~hhcl //α
+
+// wird nur im Fall obhilfe==3 nicht aufgerufen
+void hhcl::virtlieskonfein()
+{
+	const int altobverb=obverb;
+	//	obverb=1;
+	hLog(violetts+Txk[T_virtlieskonfein]+schwarz);
+	hcl::virtlieskonfein(); //ω
+	hLog(violetts+Txk[T_Ende]+Txk[T_virtlieskonfein]+schwarz); //α
+	obverb=altobverb;
+} // void hhcl::virtlieskonfein() //ω
+int main(int argc,char** argv) //α
+{
+	if (argc>1) { //ω
+	} //α
+	hhcl hhi(argc,argv); // hiesige Hauptinstanz, mit lngzuw, setzlog und pruefplatte
+	hhi.lauf(); // Einleitungsteil mit virtuellen Funktionen, 
+	// mit virtVorgbAllg,pvirtVorgbSpeziell,initopt,parsecl,pvirtmacherkl,zeighilfe,virtlieskonfein,verarbeitkonf,lieszaehlerein,MusterVorgb,dovi,dovs,virtzeigversion
+	// virtautokonfschreib,update,
+	return hhi.retu;
+} // int main 
+
+void hhcl::virttesterg()
+{
+} //ω
